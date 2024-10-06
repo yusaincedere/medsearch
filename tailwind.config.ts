a95cf3c08e3f9d96be9ca9cda3d191a372c34c7a
yusaincedere <yusaincedere@gmail.com>
2024-10-07 01:47:29 +0300

    Arama sayfası oluşturuldu

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: 'class', // Make sure this is set to 'class'
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +10,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        background: "var(--background)", // Use custom variables
         foreground: "var(--foreground)",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
